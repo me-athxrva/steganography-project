b1793d4a6cad23811dabb2cb9beba7380efe5ef8
Atharva Deore <112089821+me-athxrva@users.noreply.github.com>
2025-04-14 19:31:10 +0530

    fixed: app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ app.register_blueprint(user_blueprint, url_prefix='/')
 app.register_blueprint(image_blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
